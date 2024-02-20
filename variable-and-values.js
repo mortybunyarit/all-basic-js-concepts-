f8d942f884ccf60dec7fa37d2d53df2addeb9dfb
mortybunyarit <bunyarit.saelim@gmail.com>
2024-02-20 17:42:27 +0700

    feat:string-template-literal-concatination-practice

---
+++ variable-and-values.js
@@ -0,0 +1,13 @@
+let firstName = "John";
+
+firstName = "jane";
+
+let lastName = "Doe";
+
+const country = "Thailand";
+
+let userDetail = " Hi, I'm " + firstName + " " + lastName + " from " + country;
+
+console.log(" Hi, I'm " + firstName + " " + lastName + " from " + country);
+console.log(` Hi, I'm ${firstName} ${lastName} from ${country} `);
+console.log(userDetail);
